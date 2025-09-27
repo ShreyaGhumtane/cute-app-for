daa833668cfe1203d09e307362a8ee875c37e477
ShreyaGhumtane <149469608+ShreyaGhumtane@users.noreply.github.com>
2025-09-27 19:21:10 +0530

    Added files to public

--- src/main/java/com/cute/dearapp/controller/LoginController.java
+++ src/main/java/com/cute/dearapp/controller/LoginController.java
@@ -1,28 +1,27 @@
 package com.cute.dearapp.controller;
 
-import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+
     @GetMapping("/login")
-    public String loginPage(){
-        return "login";
+    public String loginPage() {
+        return "login"; // load login.html from templates
     }
 
-    //hardcode fo ronly one user
+    // Hardcoded login for one user
     @PostMapping("/login")
-    public String doLogin(@RequestParam String username,@RequestParam String password, Model model){
-        if("sai".equals(username) && "dudu".equals(password)){
-            return "redirect:/teddy";
-        }
-        else{
+    public String doLogin(@RequestParam String username, @RequestParam String password, Model model) {
+        if ("sai".equals(username) && "dudu".equals(password)) {
+            return "redirect:/teddy"; // go to teddy page
+        } else {
             model.addAttribute("error", "Oops! Wrong credentials 🥺");
-            return "login"; //back to login page
+            return "login"; // back to login page
         }
     }
-
 }
